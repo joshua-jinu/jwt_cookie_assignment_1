959b24ad350d9e22586aa6070e276599b87dc25a
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-02-18 09:57:53 +0530

    added jwt enryption and dcryption

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const encrypt = (payload) => {
   // encrypt the payload and return token
-}
+  const token = jwt.sign(payload, process.env.SECRET_KEY);
+  return token;
+};
 
 const decrypt = (token) => {
   // return decoded payload
-}
+  const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  if (decoded) {
+    return decoded;
+  } else {
+    return false;
+  }
+};
 
 module.exports = {
   encrypt,
-  decrypt
-}
+  decrypt,
+};
